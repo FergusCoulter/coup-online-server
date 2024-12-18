826e57e9a924aef62ccc953f54b15628b68d9a66
Fergus Coulter <41594162+FergusCoulter@users.noreply.github.com>
2024-12-18 01:03:38 +0000

    implemented env file

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
 
+import './loadEnv.js'
+
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server,{
+    cors: {
+        origin: 'http://localhost:5173',
+    }
+});
+
+
 
 import { createClient } from '@supabase/supabase-js'
+import {config} from "dotenv";
+
 const supabaseUrl = 'https://nwtapcoruuxdisphqvaz.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
@@ -16,9 +26,12 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 
 io.on('connection', async (socket) => {
     console.log('a user connected');
+
     const { data, error } = await supabase
-        .from('rooms')
+        .from('room')
         .select();
+
+    console.log(data)
 });
 
 server.listen(3000, () => {
